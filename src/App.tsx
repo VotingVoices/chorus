ad0c18fec989a1d33ac28d0237f127b11cc56421
Andy Brauninger <andy@andybrauninger.com>
2018-09-08 13:47:09 -0700

    Adding a placeholder DotNavigationBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import DotNavigationBar from './DotNagivationBar';
 import Header from './Header';
 
 import logo from './logo.svg';
@@ -11,11 +12,12 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <Header name="REACT" />
         </header>
+        <DotNavigationBar />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Header name="REACT" />
       </div>
     );
   }
---
+++ src/DotNagivationBar.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const DotNavigationBar = () => (
+	<p>This is dot navigation!</p>
+);
+
+export default DotNavigationBar;
