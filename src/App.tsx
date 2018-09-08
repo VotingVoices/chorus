b07e4c1e8f5eea48a4bc31cbbab408fc954c5a6b
Andy Brauninger <andy@andybrauninger.com>
2018-09-08 14:11:29 -0700

    Borrowing ProgressBar.js code from CrushTheMidterms.org

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <DotNavigationBar />
+        <DotNavigationBar currentStep={2} />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/DotNagivationBar.tsx
+++ src/DotNagivationBar.tsx
@@ -1,7 +1,39 @@
 import * as React from 'react';
 
-const DotNavigationBar = () => (
-	<p>This is dot navigation!</p>
-);
+function stepFill(current: number, target: number) {
+	return "E8E8E8";
+}
+
+interface IDotNavigationBarProps {
+	currentStep: number;
+}
+
+// SVG code originally borrowed from https://www.crushthemidterms.org/
+const DotNavigationBar: React.SFC<IDotNavigationBarProps> = (props: IDotNavigationBarProps) => {
+	const {currentStep} = props;
+
+	return (
+		<svg height={18} viewBox="0 0 359 18">
+			<g fill="none" fillRule="evenodd">
+				<path id="line-bg" fill="#E8E8E8" d="M4 7h346.5v3H4z" />
+
+				<path id="line-1" fill={stepFill(currentStep, 2)} d="M7 7h56.5v3H7z" />
+				<path id="line-2" fill={stepFill(currentStep, 3)} d="M66 7h56.5v3H66z" />
+				<path id="line-3" fill={stepFill(currentStep, 4)} d="M123 7h56.5v3H123z" />
+				<path id="line-4" fill={stepFill(currentStep, 5)} d="M180 7h56.5v3H180z" />
+				<path id="line-5" fill={stepFill(currentStep, 6)} d="M237 7h56.5v3H237z" />
+				<path id="line-6" fill={stepFill(currentStep, 7)} d="M294 7h56.5v3H294z" />
+
+				<ellipse id="oval-1" fill={stepFill(currentStep, 1)} cx={8.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-2" fill={stepFill(currentStep, 2)} cx={65.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-3" fill={stepFill(currentStep, 3)} cx={122.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-4" fill={stepFill(currentStep, 4)} cx={179.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-5" fill={stepFill(currentStep, 5)} cx={236.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-6" fill={stepFill(currentStep, 6)} cx={293.5} cy={8.5} rx={8.5} ry={8.5} />
+				<ellipse id="oval-7" fill={stepFill(currentStep, 7)} cx={350.5} cy={8.5} rx={8.5} ry={8.5} />
+			</g>
+		</svg>
+	);
+};
 
 export default DotNavigationBar;
