35ffbb8e660b270ee5f235e9b95f01b5db18d9de
Andy Brauninger <andy@andybrauninger.com>
2018-09-08 21:31:38 -0700

    Removing the temporary 'Header' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import './App.css';
 import { DotNavigationBar } from './components/DotNavigationBar';
-import Header from './Header';
 
 class App extends React.Component {
   public render() {
@@ -13,7 +12,6 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Header name="REACT" />
       </div>
     );
   }
