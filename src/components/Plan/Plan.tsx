a4e62dc8f2467c9438b4c8f01bfa1b95bb0b9e81
Andy Brauninger <andy@andybrauninger.com>
2018-09-18 11:33:31 -0700

    Adding preliminary query-string parsing to the 'Plan' component

--- src/components/Plan/Plan.tsx
+++ src/components/Plan/Plan.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import * as queryString from 'query-string';
 
-export class Plan extends React.Component {
+export class Plan extends React.Component<RouteComponentProps<any>, any> {
   public render() {
+    const queryValues = queryString.parse(this.props.location.search);
+
     return (
       <div className="App">
         <p>This is your voting plan.</p>
+        <p>Q1: {queryValues.q1}</p>
       </div>
     );
   }
