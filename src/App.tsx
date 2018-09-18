5cbcdffe30e42c0aed0512578dfe3b14c0e8f580
Andy Brauninger <andy@andybrauninger.com>
2018-09-18 11:22:35 -0700

    Adding a separate 'Plan' page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Plan } from './components/Plan';
 import { Questionnaire } from './components/Questionnaire';
 
 class App extends React.Component {
   public render() {
     return (
       <Router>
-        <Route exact={true} path="/" component={Questionnaire} />
+        <div>
+          <Route exact={true} path="/" component={Questionnaire} />
+          <Route path="/plan" component={Plan} />
+        </div>
       </Router>
     );
   }
---
+++ src/components/Plan/Plan.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Plan extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <p>This is your voting plan.</p>
+      </div>
+    );
+  }
+}
