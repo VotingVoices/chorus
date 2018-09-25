d469c69ea5614fb18df0a9cd1b155313e95c0b37
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 16:22:59 -0700

    Switch indentation to tabs in Questionnaire.tsx

--- src/components/Questionnaire/Questionnaire.tsx
+++ src/components/Questionnaire/Questionnaire.tsx
@@ -4,15 +4,15 @@ import { IConnectedReduxProps } from '../../store';
 import { QuestionCarousel } from '../QuestionCarousel';
 
 export class Questionnaire extends React.Component<IConnectedReduxProps, any> {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Voting Voices</h1>
-        </header>
-        <p className="App-intro" />
-        <QuestionCarousel {...this.props} />
-      </div>
-    );
-  }
+	public render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Voting Voices</h1>
+				</header>
+				<p className="App-intro" />
+				<QuestionCarousel {...this.props} />
+			</div>
+		);
+	}
 }
