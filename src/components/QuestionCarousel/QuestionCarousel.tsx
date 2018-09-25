04f0733e2255fc7d4c1e1852e9d7557b0dec6ca4
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 12:58:27 -0700

    Convert indentation to tabs in QuestionCarousel.tsx

--- src/components/QuestionCarousel/QuestionCarousel.tsx
+++ src/components/QuestionCarousel/QuestionCarousel.tsx
@@ -10,47 +10,46 @@ import { QUESTIONS, IQuestion, IQuestionnaireState } from '../../store';
 import './QuestionCarousel.css';
 
 interface IPropsFromState {
-    question: IQuestion;
-    dotNavStep: number;
-    transitionName: string;
+	question: IQuestion;
+	dotNavStep: number;
+	transitionName: string;
 }
 
 const forwardTransitionName: string = "carousel-cross-fade";
 const backTransitionName: string = "reverse-carousel-cross-fade";
 
 class InternalQuestionCarousel extends React.Component<IConnectedReduxProps & IPropsFromState, any> {
-      public render(): JSX.Element {
-          return (
-              <div>
-                  <ReactCSSTransitionReplace
-                      transitionName={this.props.transitionName}
-                      transitionEnterTimeout={1000}
-                      transitionLeaveTimeout={400} >
-                      <Question
-                          {...this.props}
-                          key={this.props.question.id}
-                          questionId={this.props.question.id}
-                          answers={QUESTIONS.find(q => q.id === this.props.question.id)!.answers} />
-                  </ReactCSSTransitionReplace>
-                  <DotNavigationBar
-                      stepCount={10}
-                      currentStep={this.props.dotNavStep}
-                      viewboxWidth={480}
-                      viewboxHeight={20}
-                      color='#E8E8E8'
-                      intervalWidth={50}
-                      strokeWidth={3}
-                      dotRadius={8} />
-                </div>
-          );
-      }
+	public render(): JSX.Element {
+		return (
+			<div>
+				<ReactCSSTransitionReplace
+					transitionName={this.props.transitionName}
+					transitionEnterTimeout={1000}
+					transitionLeaveTimeout={400} >
+					<Question
+						{...this.props}
+						key={this.props.question.id}
+						questionId={this.props.question.id}
+						answers={QUESTIONS.find(q => q.id === this.props.question.id)!.answers} />
+				</ReactCSSTransitionReplace>
+				<DotNavigationBar
+					stepCount={10}
+					currentStep={this.props.dotNavStep}
+					viewboxWidth={480}
+					viewboxHeight={20}
+					color='#E8E8E8'
+					intervalWidth={50}
+					strokeWidth={3}
+					dotRadius={8} />
+			</div>
+		);
+	}
 }
 
-
 const mapStateToProps = (state: IQuestionnaireState) => ({
-    dotNavStep: state.dotNavStep,
-    question: QUESTIONS.find(q => q.id === state.currentQuestionId)!,
-    transitionName: state.mostRecentActionWasBackButton ? backTransitionName : forwardTransitionName,
+	dotNavStep: state.dotNavStep,
+	question: QUESTIONS.find(q => q.id === state.currentQuestionId)!,
+	transitionName: state.mostRecentActionWasBackButton ? backTransitionName : forwardTransitionName,
 });
 
 export const QuestionCarousel = connect(mapStateToProps)(InternalQuestionCarousel);
