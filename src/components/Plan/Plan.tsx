c6d8a2916bf6d69d95ceb353a926b3533958ac61
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 15:45:08 -0700

    Fix indenting in Plan.tsx

--- src/components/Plan/Plan.tsx
+++ src/components/Plan/Plan.tsx
@@ -15,18 +15,18 @@ class InternalPlan extends React.Component<IPropsFromState, any> {
 				<p>This is your voting plan.</p>
 				<div>
 					{ALL_QUESTION_IDS.map(
-							(questionId: QuestionId) => {
-									const questionLabel = getQuestionFullLabel(questionId);
-									const answer = this.props.answers.find(qa => qa.questionId === questionId);
+						(questionId: QuestionId) => {
+							const questionLabel = getQuestionFullLabel(questionId);
+							const answer = this.props.answers.find(qa => qa.questionId === questionId);
 
-									if (answer !== undefined) {
-										const answerLabel = getAnswerLabel(answer!.answerId);
-										return <p key={questionId}>{questionLabel} = {answerLabel}</p>
-									}
-									else {
-										return <div key={questionId} />
-									}
+							if (answer !== undefined) {
+								const answerLabel = getAnswerLabel(answer!.answerId);
+								return <p key={questionId}>{questionLabel} = {answerLabel}</p>
 							}
+							else {
+								return <div key={questionId} />
+							}
+						}
 					)}
 				</div>
 			</div>
