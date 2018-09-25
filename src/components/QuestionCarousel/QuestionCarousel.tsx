7f3228e471f21a9f8fd02f09763c1c6ea2aef8de
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 12:52:38 -0700

    Remove dead code from QuestionCarousel.tsx

--- src/components/QuestionCarousel/QuestionCarousel.tsx
+++ src/components/QuestionCarousel/QuestionCarousel.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactCSSTransitionReplace from 'react-css-transition-replace';
-import { /*Location, Action,*/ UnregisterCallback } from 'history';
 import { connect} from 'react-redux';
 
-import { IConnectedReduxProps/*, QuestionId*/ } from '../../store';
+import { IConnectedReduxProps } from '../../store';
 import { Question } from '../Question';
-// import { IAnswerProps } from '../Answer';
 import { DotNavigationBar } from '../DotNavigationBar';
 import { QUESTIONS, IQuestion, IQuestionnaireState } from '../../store';
 
@@ -20,21 +18,11 @@ export interface IQuestionCarouselState {
     transitionName: string;
 }
 
-// TODO: Remove
-/*
-function getQuestionIdFromPath(path: string): QuestionId {
-    const questionIdStr = path.slice(1, path.length);
-    return questionIdStr as QuestionId;
-}
-*/
-
 const forwardTransitionName: string = "carousel-cross-fade";
 // const backTransitionName: string = "reverse-carousel-cross-fade";
 
 class InternalQuestionCarousel extends React.Component<IConnectedReduxProps & IPropsFromState, any>
 {
-    private historyUnregister: UnregisterCallback | undefined;
-
     constructor(props: IConnectedReduxProps & IPropsFromState, context?: any) {
         super(props, context);
 
@@ -58,21 +46,6 @@ class InternalQuestionCarousel extends React.Component<IConnectedReduxProps & IP
             this.props.history.push({ pathname: questionId });
         }
         */
-    }
-
-    public componentDidMount() {
-        // TODO: Remove
-        /*
-        this.historyUnregister = this.props.history.listen((location, action) => {
-            // this._locationChanged(location, action);
-        });
-        */
-    }
-
-    public componentWillUnmount() {
-        if (this.historyUnregister !== undefined) {
-            this.historyUnregister();
-        }
     }
 
       public render(): JSX.Element {
@@ -100,67 +73,6 @@ class InternalQuestionCarousel extends React.Component<IConnectedReduxProps & IP
                 </div>
           );
       }
-
-      // TODO: Remove
-      /*
-    private _onQuestionAnswered = (ev?: React.FormEvent<HTMLElement | HTMLInputElement>, answer?: IAnswerProps) => {
-        const { questions } = this.props;
-        const currentQuestion = questions.find(q => q.id === this.state.currentQuestionId);
-
-        const answers = this.state.answers;
-        const answerId = answer!.answerId;
-
-        // TODO: There's probably a more elegant way to do this...
-        const existingAnswer = answers.find(qa => qa.questionId === currentQuestion!.id);
-        if (existingAnswer === undefined) {
-            answers.push({ questionId: currentQuestion!.id, answerId });
-        }
-        else {
-            existingAnswer!.answerId = answerId;
-        }
-
-        const nextQuestionId = currentQuestion!.nextQuestionId(answerId);
-
-        if (nextQuestionId === QuestionId.END_OF_QUESTIONS) {
-            // Unregister from history so that we don't get confused inside our own location-changed handler.
-            this.historyUnregister!();
-            this.historyUnregister = undefined;
-
-            // Redirect to the plan page.
-            this.setState({
-                answers,
-            });
-        }
-        else {
-            this.props.history.push({ pathname: nextQuestionId });
-
-            this.setState({
-                answers,
-                currentQuestionId: nextQuestionId,
-                currentDotNavStep: questions.find(q => q.id === nextQuestionId)!.dotNavStep,
-            });
-        }
-    }
-
-    private _locationChanged(location: Location, action: Action)
-    {
-        const path = this.props.history.location.pathname;
-        const questionIdStr = path.slice(1, path.length);
-        const newQuestionId = questionIdStr as QuestionId;
-        const { questions } = this.props;
-
-        let transitionName = forwardTransitionName;
-        if (action === 'POP') {
-            transitionName = backTransitionName;
-        }
-
-        this.setState({
-            currentQuestionId: newQuestionId,
-            currentDotNavStep: questions.find(q => q.id === newQuestionId)!.dotNavStep,
-            transitionName,
-        });
-    }
-    */
 }
 
 
