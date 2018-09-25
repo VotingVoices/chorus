2dd41c87d171beb8d3010edfd920a9a117431940
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 15:39:20 -0700

    Use an animation to transition to the Plan page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as ReactCSSTransitionReplace from 'react-css-transition-replace';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -11,16 +12,31 @@ interface IPropsFromState {
 }
 
 class App extends React.Component<IConnectedReduxProps & IPropsFromState> {
-	public render() {
+	public render(): JSX.Element {
+		return (
+			<ReactCSSTransitionReplace
+				transitionName="carousel-cross-fade"
+				transitionEnterTimeout={1000}
+				transitionLeaveTimeout={400} >
+				{this.renderViewSpecificContent()}
+			</ReactCSSTransitionReplace>
+		);
+	}
+
+	private renderViewSpecificContent(): JSX.Element {
 		const { currentView } = this.props;
 
 		switch (currentView) {
 			case AppView.Questionnaire: {
-				return (<Questionnaire {...this.props} />);
+				return (<Questionnaire key="questionnaire" {...this.props} />);
 			}
 
 			case AppView.Plan: {
-				return (<Plan {...this.props} />);
+				return (<Plan key="plan" {...this.props} />);
+			}
+
+			default: {
+				throw new Error("Unrecognized AppView.");
 			}
 		}
 	}
