925d56cd08f4e8830b7c1424a77c222919c60c69
Andy Brauninger <andy@andybrauninger.com>
2018-09-24 21:21:18 -0700

    Remove temporary 'Dot nav step' indicator from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,37 +10,27 @@ import { Plan } from './components/Plan';
 import { Questionnaire } from './components/Questionnaire';
 import { IQuestionnaireState } from './store';
 
-interface IPropsFromState {
-	dotNavStep: number;
-}
-
 interface IAppProps {
 	store: Store<IQuestionnaireState>;
 	history: History;
 }
 
-class App extends React.Component<IPropsFromState & IAppProps> {
+class App extends React.Component<IAppProps> {
 	public render() {
-		const { dotNavStep, history } = this.props;
+		const { history } = this.props;
 
 		return (
-			<React.Fragment>
-				<ConnectedRouter history={history}>
-					<Switch>
-						<Route exact={true} path="/" component={Questionnaire} />
-						<Route path="/plan*" component={Plan} />
-						<Route path="/*" component={Questionnaire} />
-					</Switch>
-				</ConnectedRouter>
-
-				<p>Nav step: {dotNavStep}</p>
-			</React.Fragment>
+			<ConnectedRouter history={history}>
+				<Switch>
+					<Route exact={true} path="/" component={Questionnaire} />
+					<Route path="/plan*" component={Plan} />
+					<Route path="/*" component={Questionnaire} />
+				</Switch>
+			</ConnectedRouter>
 		);
 	}
 }
 
-const mapStateToProps = ({ dotNavStep }: IQuestionnaireState) => ({
-	dotNavStep
-})
+const mapStateToProps = (state: IQuestionnaireState) => ({ })
 
 export default connect(mapStateToProps)(App);
