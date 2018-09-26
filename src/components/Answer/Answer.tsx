66c93d78da21c105f1a3f5d14c9732657e6081de
Andy Brauninger <andy@andybrauninger.com>
2018-09-25 19:44:26 -0700

    Merging AnswerTypes.ts into Answer.tsx

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,6 +1,101 @@
 import * as React from 'react';
+
+import { AnswerId } from '../../store';
+
 import './Answer.css';
-import { getAnswerLabel, IAnswerProps } from './AnswerTypes';
+
+// TODO: Move to its own file.
+export function getAnswerLabel(answer: AnswerId): string {
+    switch(answer) {
+        case AnswerId.Yes:
+            return 'Yep!';
+
+        case AnswerId.No:
+            return 'Nope';
+
+        case AnswerId.EmphaticYes:
+            return 'Heck yeah!';
+
+        case AnswerId.UncertainNo:
+            return 'Uhh... Nope';
+
+        case AnswerId.DontKnow:
+            return 'I dunno...';
+
+        case AnswerId.Other:
+            return 'Other';
+
+        case AnswerId.Washington:
+            return 'Washington';
+
+        case AnswerId.Oregon:
+            return 'Oregon';
+
+        case AnswerId.Colorado:
+            return 'Colorado';
+
+        case AnswerId.OtherState:
+            return 'Nope';
+
+        case AnswerId.WalkOrBike:
+            return 'Walk or bike';
+
+        case AnswerId.DriveMyself:
+            return 'Drive myself';
+
+        case AnswerId.RideShare:
+            return 'Lyft or Uber';
+
+        case AnswerId.AskSomeoneForRide:
+            return 'Ask someone for a ride';
+
+        case AnswerId.FamilyMembers:
+            return 'Family members';
+
+        case AnswerId.Coworkers:
+            return 'Coworkers';
+
+        case AnswerId.Friends:
+            return 'Friends';
+
+        case AnswerId.SomeoneElse:
+            return 'Someone else';
+        
+        case AnswerId.NoThanks:
+            return 'No thanks';
+
+        case AnswerId.Excited:
+            return 'Excited';
+
+        case AnswerId.Happy:
+            return 'Happy';
+
+        case AnswerId.Nervous:
+            return 'Nervous';
+
+        case AnswerId.Shocked:
+            return 'Shocked';
+
+        case AnswerId.Meh:
+            return 'Meh';
+
+        case AnswerId.Worried:
+            return 'Worried';
+
+        case AnswerId.Sad:
+            return 'Sad';
+
+        case AnswerId.Angry:
+            return 'Angry';
+    }
+
+    throw new Error("Unrecognized AnswerId");
+}
+
+interface IAnswerProps extends React.InputHTMLAttributes<HTMLElement | HTMLInputElement>
+{
+    answerId: AnswerId;
+}
 
 export class Answer extends React.Component<IAnswerProps, any>
 {
