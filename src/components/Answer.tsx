8bd8fb38653454ca358dcdf098456e66070afa28
Andy Brauninger <andy@andybrauninger.com>
2018-09-26 09:13:41 -0700

    Cosmetic changes to Answer.tsx

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -5,33 +5,30 @@ import { getAnswerLabel } from '../strings';
 
 import './Answer.css';
 
-interface IAnswerProps extends React.InputHTMLAttributes<HTMLElement | HTMLInputElement>
-{
-    answerId: AnswerId;
+interface IAnswerProps extends React.InputHTMLAttributes<HTMLElement | HTMLInputElement> {
+	answerId: AnswerId;
 }
 
-export class Answer extends React.Component<IAnswerProps, any>
-{
-    public render(): JSX.Element
-    {
-        const { answerId } = this.props;
-        const label = getAnswerLabel(answerId);
+export class Answer extends React.Component<IAnswerProps, any> {
+	public render(): JSX.Element {
+		const { answerId } = this.props;
+		const label = getAnswerLabel(answerId);
 
-        return (
-            <span className="dot" onClick={this._onClick}>
-                {label}
-            </span>
-        );
-    }
+		return (
+			<span className="dot" onClick={this._onClick}>
+				{label}
+			</span>
+		);
+	}
 
-    private _onClick = (ev: React.MouseEvent<HTMLElement>): void => {
-        const { onClick } = this.props;
-        ev.preventDefault();
-        ev.stopPropagation();
+	private _onClick = (ev: React.MouseEvent<HTMLElement>): void => {
+		const { onClick } = this.props;
+		ev.preventDefault();
+		ev.stopPropagation();
 
-        if (onClick)
-        {
-            onClick(ev);
-        }
-    }
+		if (onClick)
+		{
+			onClick(ev);
+		}
+	}
 }
