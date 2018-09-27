02253d852353c4808a90b8a3806565dc3a25935b
Andy Brauninger <andy@andybrauninger.com>
2018-09-27 10:14:26 -0700

    Add the StartOverButton to the Plan page

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
+import { connect} from 'react-redux';
+import { StartOverButton } from './StartOverButton';
 import { getAnswerLabel, getQuestionFullLabel } from '../strings';
-import { ALL_QUESTION_IDS, IQuestionAndAnswer, QuestionId } from '../store';
+import { ALL_QUESTION_IDS, IConnectedReduxProps, IQuestionAndAnswer, QuestionId } from '../store';
 
 interface IPlanProps {
 	answers: IQuestionAndAnswer[],
 }
 
-export class Plan extends React.Component<IPlanProps, any> {
+class InternalPlan extends React.Component<IPlanProps & IConnectedReduxProps, any> {
 	public render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<p>This is your voting plan.</p>
 				</header>
+
+				<StartOverButton {...this.props} />
+
 				<div>
 					{ALL_QUESTION_IDS.map(
 						(questionId: QuestionId) => {
@@ -33,3 +38,8 @@ export class Plan extends React.Component<IPlanProps, any> {
 		);
 	}
 }
+
+const mapStateToProps = () => ({
+});
+
+export const Plan = connect(mapStateToProps)(InternalPlan);
