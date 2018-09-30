68d484d3f52b7677bece055e6c70857ad0975294
David-Liu-G <david.liu.g@outlook.com>
2018-09-30 01:36:53 -0700

    Added try catch error and enable cors

--- src/api/SaveSurveyResult.js
+++ src/api/SaveSurveyResult.js
@@ -4,7 +4,11 @@ exports.handler = function (event, context, callback) {
     if (event.hasOwnProperty("body")) {
         let request = JSON.parse(event.body);
         let surveyDAO = new SurveyDAO();
-        surveyDAO.saveSurvey(request);
+        try {
+            surveyDAO.saveSurvey(request);
+        } catch (error) {
+            callback(error);
+        }
     } else {
         callback("Post request is empty");
     }
