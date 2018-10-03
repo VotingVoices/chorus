6f57c18fab6146672ee72a6175a1158119d3dfc6
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-03 09:25:44 -0700

    Remove the About and Donate buttons for now, since they don't work

--- src/components/HeaderNavigationBar.tsx
+++ src/components/HeaderNavigationBar.tsx
@@ -16,14 +16,8 @@ export class HeaderNavigationBar extends React.Component<any, any> {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight={true}>
-                        <NavItem eventKey={1} href="#/About">
-                            About
-                        </NavItem>
                         <NavItem eventKey={2} href="mailto:info@votingvoices.org">
                             Contact
-                        </NavItem>
-                        <NavItem eventKey={3} href="#/Donate">
-                            Donate
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>	
