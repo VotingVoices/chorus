8f2cb0a2a1c60bb2c75367a212d103336484fe83
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-03 21:15:26 -0700

    Tweak the styling of the dot navigation

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -41,8 +41,8 @@ class InternalSurvey extends React.Component<IConnectedReduxProps & IPropsFromSt
 						stepCount={PLAN_DOT_NAV_STEP - 1}
 						currentStep={this.props.dotNavStep}
 						color='#E8E8E8'
-						intervalWidth={50}
-						strokeWidth={3}
+						intervalWidth={32}
+						strokeWidth={4}
 						dotRadius={8} />
 				</div>
 
