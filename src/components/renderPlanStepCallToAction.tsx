34303edcce607ebdcf0168297731f79d8e0bb557
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-10 16:14:24 -0700

    Remove a resolved TODO

--- src/components/renderPlanStepCallToAction.tsx
+++ src/components/renderPlanStepCallToAction.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 export function renderPlanStepCallToAction(callToAction: string | undefined, link: string | undefined): JSX.Element {
 	if (callToAction !== undefined && link !== undefined) {
-		// TODO: Link should open in a new tab.
 		return <div className="plan-step-call-to-action VotingVoices-sans-serif"><a href={link!} target="_blank">{callToAction!}</a></div>
 	}
 	else {
