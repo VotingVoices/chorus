d9b976a1d52b3a0101a5deec3df790eb42a28dc4
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-18 01:45:36 -0700

    Switch indentation to tabs in Question.tsx

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -9,54 +9,54 @@ import { getQuestionFullLabel, StringId } from '../strings';
 import './Question.css';
 
 interface IQuestionProps {
-    questionId: QuestionId,
-    answers: AnswerId[],
+	questionId: QuestionId,
+	answers: AnswerId[],
 }
 
 interface IPropsFromState {
-    getString: (id: StringId) => string;
+	getString: (id: StringId) => string;
 }
 
 interface IPropsFromDispatch {
-    answerQuestion: typeof answerQuestion,
+	answerQuestion: typeof answerQuestion,
 }
 
 class Question extends React.Component<IQuestionProps & IConnectedReduxProps & IPropsFromState & IPropsFromDispatch, any> {
-    public render(): JSX.Element {
-        const { questionId, answers } = this.props;
-        const label = this.props.getString(getQuestionFullLabel(questionId));
+	public render(): JSX.Element {
+		const { questionId, answers } = this.props;
+		const label = this.props.getString(getQuestionFullLabel(questionId));
 
-        return (
-            <div>
-                <div className="question-label VotingVoices-serif">{label}</div>
-                <div className="answer-group">
-                    {answers.map(
-                        (answerId: AnswerId) => {
-                            return (<Answer onClick={this._onClick(answerId)} answerId={answerId} key={answerId} />);
-                        }
-                    )}
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div>
+				<div className="question-label VotingVoices-serif">{label}</div>
+				<div className="answer-group">
+					{answers.map(
+						(answerId: AnswerId) => {
+							return (<Answer onClick={this._onClick(answerId)} answerId={answerId} key={answerId} />);
+						}
+					)}
+				</div>
+			</div>
+		);
+	}
 
-    private _onClick = (answerId: AnswerId) => {
-        return (ev: React.MouseEvent<HTMLElement | HTMLInputElement>) => {
-            const { questionId, answers } = this.props;
+	private _onClick = (answerId: AnswerId) => {
+		return (ev: React.MouseEvent<HTMLElement | HTMLInputElement>) => {
+			const { questionId, answers } = this.props;
 
-            const answer = answers.find((o: AnswerId) => o === answerId );
+			const answer = answers.find((o: AnswerId) => o === answerId );
 
-            this.props.answerQuestion(questionId, answer!); 
-        };
-    };
+			this.props.answerQuestion(questionId, answer!); 
+		};
+	};
 }
 
 const mapStateToProps = (state: IQuestionnaireState) => ({
-    getString: state.getString,
+	getString: state.getString,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    answerQuestion: (questionId: QuestionId, answerId: AnswerId) => dispatch(answerQuestion(questionId, answerId)),
+	answerQuestion: (questionId: QuestionId, answerId: AnswerId) => dispatch(answerQuestion(questionId, answerId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Question);
