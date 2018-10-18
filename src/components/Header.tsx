39c7701c139c08e19a891b7d9124c49b8af5542f
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-17 22:57:57 -0700

    Add a deadline banner to the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,18 @@ import vvlogo from './vvlogo.png';
 export class Header extends React.Component<any, any> {
 	public render() {
 		return (
-			<div className="vv-page-header">
-				<div className="header-logo">
-					<a href="#/LandingPage"><img src={vvlogo} /></a>
+			<div>
+				<div className="vv-page-header">
+					<div className="header-logo">
+						<a href="#/LandingPage"><img src={vvlogo} /></a>
+					</div>
+					<div className="right-buttons">
+						<Button type="button" className="vv-button contact-about-button" href="mailto:info@votingvoices.org">Contact</Button>
+					</div>
 				</div>
-				<div className="right-buttons">
-					<Button type="button" className="vv-button contact-about-button" href="mailto:info@votingvoices.org">Contact</Button>
+
+				<div>
+					<strong>Make a plan to vote.</strong> The midterm elections are on <strong>November 6th</strong>.
 				</div>
 			</div>
 		);
