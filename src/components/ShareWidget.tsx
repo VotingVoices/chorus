85745dba107e96ad2ffc9cba03f2bb2c849e6172
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-19 22:07:05 -0700

    Use a larger size for the share widgets

--- src/components/ShareWidget.tsx
+++ src/components/ShareWidget.tsx
@@ -9,9 +9,11 @@ export class ShareWidget extends React.Component<any, any> {
 			<div className="share-widget-root">
 				<div className="fb-share-button" 
 					data-href="https://votingvoices.org" 
-					data-layout="button_count" />
+					data-layout="button_count"
+					data-size="large" />
 				<a className="twitter-share-button"
 					data-url="http://votingvoices.org/voteplan"
+					data-size="large"
 					href="https://twitter.com/intent/tweet?text=Check%20out%20Voting%20Voices%20to%20create%20a%20personalized%20vote%20plan%20in%20minutes!">
 					Tweet
 				</a>
