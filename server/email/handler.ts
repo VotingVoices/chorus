6cac60beefde2e0da074c3ec1ae90e0674bba019
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-20 23:33:30 -0700

    Attempt to send an email through SES. (Doesn't appear to work yet.)

--- server/email/handler.ts
+++ server/email/handler.ts
@@ -1,11 +1,39 @@
 import { Handler, Context, Callback } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({region: 'us-east-1'});
 
 interface IHelloResponse {
 	statusCode: number,
 	body: string,
 }
 
-const hello: Handler = (_event: any, _context: Context, callback: Callback) => {
+const sendVotePlanEmail: Handler = (_event: any, _context: Context, callback: Callback) => {
+	const sendParams: AWS.SES.SendEmailRequest = {
+		Source: 'andy@andybrauninger.com',
+		Destination: {
+			ToAddresses: [
+				'andy@andybrauninger.com',
+			]
+		},
+		Message: {
+			Subject: {
+				Charset: 'UTF-8',
+				Data: 'Test email from the lambda',
+			},
+			Body: {
+				Text: {
+					Charset: 'UTF-8',
+					Data: 'Four score and seven lambdas ago...",'
+				},
+			}
+		}
+	};
+
+	new AWS.SES({ apiVersion: '2010-12-01'}).sendEmail(sendParams);
+
+	console.log("Just attempted to send an email.");
+	
 	const response: IHelloResponse = {
 		statusCode: 200,
 		body: JSON.stringify({
@@ -16,4 +44,4 @@ const hello: Handler = (_event: any, _context: Context, callback: Callback) => {
 	callback(undefined, response);
 };
 
-export { hello }
+export { sendVotePlanEmail }
